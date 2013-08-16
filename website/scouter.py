81b62250e8e44bfbc97870c715c359a27536fd40
Josh Gachnang <josh@pcsforeducation.com>
2013-08-16 16:50:19 -0500

    Analyzing whole pic from top to bottom. Realized we may be starting in a different corner.

--- website/scouter.py
+++ website/scouter.py
@@ -93,7 +93,7 @@ def percent_in_color_range(pic,x,y,color_range):
     rl, ru = color_range['r']
     gl, gu = color_range['g']
     bl, bu = color_range['b']
-    for i in range(0, int(y / 3)):
+    for i in range(0, int(y)):
         for j in range(0, int(x / 4)):
             r, g, b = pic[j * 4, i]
             # print r, g, b
