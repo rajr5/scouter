6d4b927875a4c0e3089c61d73e0f7a96e754a832
Josh Gachnang <josh@pcsforeducation.com>
2013-08-16 15:08:39 -0500

    Updating with new power levels. Just using standard Saiyan hair as the guideline. 2.5bil is a ridiculous power level (for SSJ1).

--- website/scouter.py
+++ website/scouter.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import time
 import cv2
 import cv2.cv as cv
@@ -13,63 +14,128 @@ import logging
 Rectangles are coordinates of upper left and lower right corners.
 Colors are in BGR??
 """
-
+blond_color_range = {'r': (209, 255), 'g': (137, 242), 'b': (65, 154)}
+black_color_range = {'r': (0, 40), 'g': (0, 40), 'b': (0, 40)}
+red_color_range = {'r': (123, 252), 'g': (11, 252), 'b': (51, 252)}
 
 logger = logging.getLogger('debugger')
 
 # Ratio of width to height of timeline images.
 RATIO = 0.66
 
-# def draw_rects(img, rects, color):
+"""
+General form of power level calculation:
+pl = min + (max-min/100000) * power level
+"""
+
+
+# def ssj1(percent):
 #     """
-#     Given an image, sets of rectangles, and a color, draw them on img. img will be now be the image + rectangles.
+#     Given blond percentage, return a power level. Accodring to http://dragonball.wikia.com/wiki/List_of_Power_Levels,
+#     SSJ should go from about 150mil to 2.5bil.
+#     @param percent:
+#     @type percent:
+#     @return:
+#     @rtype:
 #     """
-#     for x1, y1, x2, y2 in rects:
-#         cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+#     if percent < 1:
+#         return 0
+#     if percent > 10:
+#         percent = 10
+#     return ((25000000000/100000) * (percent ** 4))
 #
-#
-# def draw_circles(img, circles, color):
+# def kaio_ken(percent):
 #     """
-#     Given rectangles, create and draw circles with center point at the center of the rectangle and with radius = half
-#     the diagonal of the rectangle.
+#     Given the red percentage, return a power level. We see Kaio-ken power levels from 8k (normal v Vegeta)
+#     to 60mil (x20, against Frieza)
+#     @param percent:
+#     @type percent:
+#     @return:
+#     @rtype:
 #     """
-#     for rect in circles:
-#         center = center_of_rectangle(rect)
-#         radius = radius_of_rectangle(rect)
-#         cv2.circle(img, center, int(radius * .75), color, thickness=20)
-#
-#
-# def center_of_rectangle(rect):
-#     """
-#     Standard center point of rectangle formula (truncated)
-#     """
-#     x1, y1, x2, y2 = rect
-#     return int((x2 + x1) / 2), int((y2 + y1) / 2)
-#
-#
-# def distance(x1, y1, x2, y2):
-#     """
-#     Standard distance formula (truncated)
-#     """
-#     return int(math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))
-#
-#
-# def radius_of_rectangle(rect):
-#     """
-#     Given rectangle, find half the diameter (truncated)
-#     """
-#     return int(distance(rect[0], rect[1], rect[2], rect[3]) / 2)
-#
-#
-# def write_text(img, rect, text, font_color):
-#     """
-#     Try to intelligently write the give text above (preferred) or below the image.
-#     """
-#
-#     cv2.putText(
-#         img, text, (rect[0] - 100, rect[2] -
-#                     100), cv2.FONT_HERSHEY_SIMPLEX, 8.0, font_color,
-#         thickness=20, lineType=cv2.CV_AA)
+#     if percent < 1:
+#         return 0
+#     if percent > 10:
+#         percent = 10
+#     return (60000000/10000) * (percent ** 4)
+
+def saiyan(percent):
+    """
+    Given the black percentage, return a power level. We see non SSJ Saiyans from about 1k (Raditz) to let's say 8k.
+    @param percent:
+    @type percent:
+    @return:
+    @rtype:
+    """
+    if percent < 1:
+        return 0
+    if percent > 10:
+        percent = 10
+    return 1000 + ((50000/10000) * (percent ** 4))
+
+def percent_in_color_range(pic,x,y,color_range):
+    """
+    Checks each pixel to see if its rgb color is between all the upper and lower bounds in color_range. Returns
+    a percent of the image that is in the range from 0 to 100.
+    @param pic: An image loaded by PIL via Image.open(file).load()
+    @type pic: PIL loaded image
+    @param x: width in px
+    @type x: int
+    @param y: height in v
+    @type y: int
+    @param color_range: {'r': (lower bound, upperbound), 'g': (l, u), 'b': (l, u)}
+    @type color_range: dict
+    @return: percent of image that is a color from 0 to 100.
+    @rtype: float
+    """
+    total_px = 0
+    color_px = 0
+    rl, ru = color_range['r']
+    gl, gu = color_range['g']
+    bl, bu = color_range['b']
+    for i in range(0, int(y / 10)):
+        for j in range(0, int(x / 2)):
+            r, g, b = pic[j, i * 10]
+            # print r, g, b
+            # If the pix color is inside the range (inclusive), add one.
+            if rl <= r <= ru and gl <= g <= gu and bl <= b <= bu:
+                color_px += 1
+            total_px += 1
+    return color_px / total_px * 100
+
+
+
+def power_level(face):
+    """
+    Given a face file, analyze for power levels.
+    @param face:
+    @type face:
+    @return:
+    @rtype:
+    """
+
+    im = Image.open(face)
+    size_of_image = im.size
+    x = size_of_image[0]
+    y = size_of_image[1]
+
+    pic = im.load()
+    # blond = percent_in_color_range(pic, x, y, blond_color_range)
+    black = percent_in_color_range(pic, x, y, black_color_range)
+    # red = percent_in_color_range(pic, x, y, red_color_range)
+    # logger.debug("Image colors: blond {0}, black {1}".format(blond, black))
+    logger.debug("Image colors: black {0}".format(black))
+    # TODO Apply a weighting algorithm to see which type of power level we're looking at (SSJ or SSJ4).
+    # blond_power = ssj1(blond)
+    black_power = saiyan(black)
+    # red_power = kaio_ken(red)
+    # print "Blond, black, red power: ", blond_power, black_power, red_power
+    # power = max(blond_power, black_power, red_power)
+    power = black_power
+    if power < 1000:
+        return random.randint(1,206)
+    else:
+        return power
 
 
 def face_detect(img, cascade_fn='haarcascades/haarcascade_frontalface_alt.xml',
@@ -86,13 +152,6 @@ def face_detect(img, cascade_fn='haarcascades/haarcascade_frontalface_alt.xml',
         return []
     rects[:, 2:] += rects[:, :2]
     return rects
-
-
-def power_level(rect):
-    """
-    TODO Figure out power level based on how much blonde hair is in the picture.
-    """
-    return random.randrange(1, 30000)
 
 
 def slice_face(rect, img, store_faces_path):
