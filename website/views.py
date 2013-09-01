498611f4f1208b5c8486c54a5a92c82b2548529a
Josh Gachnang <josh@pcsforeducation.com>
2013-09-01 13:55:22 -0500

    Some finishing touches.

--- website/views.py
+++ website/views.py
@@ -1,30 +1,20 @@
-from django.http import HttpResponseRedirect, HttpResponseBadRequest, HttpResponseNotFound, HttpResponse
+import random
+import logging
+import json
+import os
+from django.http import HttpResponseRedirect, HttpResponseBadRequest, HttpResponse
 from django.contrib.auth.decorators import login_required
-# from django.contrib.auth import logout
 from django.shortcuts import render_to_response
-from django.core.exceptions import ObjectDoesNotExist
 from django.template import RequestContext
-from django.contrib import messages
 from django.conf import settings
-from django.db import IntegrityError
-from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
-from django.forms.models import model_to_dict
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from allauth.socialaccount.models import SocialAccount, SocialToken
 from website.models import GoogleCredential
-from website.glass.mirror import Mirror, Timeline, Contact
-import logging
-from django.views.decorators.csrf import csrf_exempt
-import json
+from website.glass.mirror import Mirror, Contact
 from scouter import scout
-import io
-from apiclient.http import MediaIoBaseUpload
-import urllib
 from glass import oauth_utils
-from django.contrib.auth import login, authenticate, logout
-import os
-import datetime
-import random
+
 
 debug_logger = logging.getLogger('debugger')
 client_secrets_filename = os.path.join(
@@ -35,88 +25,19 @@ def homepage(request):
     template_data = {}
     debug_logger.debug("User id: {0}".format(request.user.id))
     if not request.user.is_authenticated():
-        template_data['auth_url'] = oauth_utils.get_auth_url(request, client_secrets_filename=client_secrets_filename, redirect_uri=settings.GOOGLE_REDIRECT_URI)
-        return render_to_response('login.html', template_data, context_instance=RequestContext(request))
-        # return HttpResponseRedirect()
-    return HttpResponseRedirect('/install/')
+        # We need to create an auth URL for non authenticated users. They will see an "Install on Glass" button.
+        template_data['auth_url'] = oauth_utils.get_auth_url(request, client_secrets_filename=client_secrets_filename,
+                                                             redirect_uri=settings.GOOGLE_REDIRECT_URI)
+    return render_to_response('home.html', template_data, context_instance=RequestContext(request))
+
 
 def install(request):
     template_data = {}
     debug_logger.debug("User id: {0}".format(request.user.id))
     mirror = _get_mirror(request.user.id)
     _register_glass_app(mirror, request.user.id)
-    return HttpResponseRedirect('https://google.com/myglass')
-    # return render_to_response('home.html', context_instance=RequestContext(request))
-
-
-def _get_mirror(user_id):
-    """
-    Given a request, build the mirror object by getting the credentials from the DB and building it.
-    @param request:
-    @type request:
-    @return:
-    @rtype:
-    """
-    try:
-        google_credentials = GoogleCredential.objects.get(user=user_id)
-    except Exception:
-        raise ValueError("User {0} has no GoogleCredential".format(user_id))
-    oauth_credentials = google_credentials.oauth2credentials()
-    mirror = Mirror.from_credentials(oauth_credentials)
-    google_credentials.refresh(http=mirror.http)
-    return mirror
-
-
-def oauth_redirect(request):
-    return oauth_utils.process_oauth_redirect(request, client_secrets_filename=client_secrets_filename, redirect_uri=settings.GOOGLE_REDIRECT_URI)
-
-
-def _register_glass_app(mirror, id):
-    """
-    Create a Contact object and add it to the user's Glass. Then subscribe to notifications from that contact.
-    """
-    contacts = mirror.list_contacts()
-    # if len(contacts) == 0:
-    mirror.clear_contacts()
-    if settings.ENV == "production":
-        display_name = "Scouter"
-    else:
-        display_name = "Scouter Dev"
-    contact = Contact(
-        display_name=display_name, id=id, image_urls=[
-            'https://scouteronglass.com/static/img/contact_img.png'],
-        type="INDIVIDUAL", accept_types=["image/jpeg", "image/png"], priority=1)
-    mirror.post_contact(contact)
-    mirror.subscribe(callback_url='https://scouteronglass.com/mirror/subscription/reply/', subscription_type="reply", user_token=id)
-
-
-@login_required
-def clear_contacts(request):
-    credentials = _get_credentials(request.user.id)
-    mirror = _get_mirror(request.user.id)
-    mirror.clear_contacts()
-    return HttpResponse("Clear!")
-
-
-@login_required
-def callback(request):
-    pass
-
-
-@login_required
-def auth_return(request):
-    pass
-
-
-def _get_credentials(id):
-    debug_logger.info("Get credentials for user: {0}".format(id))
-    return GoogleCredential.objects.get(user=id)
-
-
-def _get_token(id):
-    account = SocialAccount.objects.get(user=id)
-    token = SocialToken.objects.get(account=account.id)
-    return token
+    # return HttpResponseRedirect('https://google.com/myglass')
+    return HttpResponseRedirect('/')
 
 
 @csrf_exempt
@@ -157,6 +78,66 @@ def subscription_reply(request):
         raise
     mirror.update_timeline(timeline)
     return HttpResponse('OK')
+
+
+def oauth_redirect(request):
+    return oauth_utils.process_oauth_redirect(request, client_secrets_filename=client_secrets_filename, redirect_uri=settings.GOOGLE_REDIRECT_URI)
+
+
+@login_required
+def clear_contacts(request):
+    credentials = _get_credentials(request.user.id)
+    mirror = _get_mirror(request.user.id)
+    mirror.clear_contacts()
+    return HttpResponse("Clear!")
+
+
+def _get_mirror(user_id):
+    """
+    Given a request, build the mirror object by getting the credentials from the DB and building it.
+    @param request:
+    @type request:
+    @return:
+    @rtype:
+    """
+    try:
+        google_credentials = GoogleCredential.objects.get(user=user_id)
+    except Exception:
+        raise ValueError("User {0} has no GoogleCredential".format(user_id))
+    oauth_credentials = google_credentials.oauth2credentials()
+    mirror = Mirror.from_credentials(oauth_credentials)
+    google_credentials.refresh(http=mirror.http)
+    return mirror
+
+
+def _register_glass_app(mirror, id):
+    """
+    Create a Contact object and add it to the user's Glass. Then subscribe to notifications from that contact.
+    """
+    contacts = mirror.list_contacts()
+    # if len(contacts) == 0:
+    mirror.clear_contacts()
+    if settings.ENV == "production":
+        display_name = "Scouter"
+    else:
+        display_name = "Scouter Dev"
+    contact = Contact(
+        display_name=display_name, id=id, image_urls=[
+            'https://scouteronglass.com/static/img/contact_img.png'],
+        type="INDIVIDUAL", accept_types=["image/jpeg", "image/png"], priority=1)
+    mirror.post_contact(contact)
+    mirror.subscribe(callback_url='https://scouteronglass.com/mirror/subscription/reply/', subscription_type="reply", user_token=id)
+
+
+def _get_credentials(id):
+    debug_logger.info("Get credentials for user: {0}".format(id))
+    return GoogleCredential.objects.get(user=id)
+
+
+def _get_token(id):
+    account = SocialAccount.objects.get(user=id)
+    token = SocialToken.objects.get(account=account.id)
+    return token
 
 
 def _create_timelines(cards, mirror, timeline_item):
